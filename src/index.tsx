66b3fbca2a9f84eed6ec9c040f3b871fcb67983c
elevensky <yh3417@gmail.com>
2020-02-12 00:34:36 +0800

    chore: 组件初始化

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+import styles from './style.css';
+
+// Delete me
+export const Thing = () => {
+  return (
+    <div className={styles.test}>the snozzberries taste like snozzberries</div>
+  );
+};
